ab42a5169614ed30b6c683f92fc5a1545fd40555
Nicole Gomez <gomez22n@mtholyoke.edu>
2018-01-16 14:51:26 -0500

    Name change

--- app/src/main/java/com/example/demouser/yikesyak/HomePage.java
+++ app/src/main/java/com/example/demouser/yikesyak/HomePage.java
@@ -3,12 +3,12 @@ package com.example.demouser.yikesyak;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class HomePage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.home_page_main);
         //change
     }
 }
