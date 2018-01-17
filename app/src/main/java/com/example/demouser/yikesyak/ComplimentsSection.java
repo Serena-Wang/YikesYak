458c70cd25ecd9cf4320d53231f64feefa830ecf
Serena Wang <wang225y@mtholyoke.edu>
2018-01-17 13:12:14 -0500

    Update Layout

---
+++ app/src/main/java/com/example/demouser/yikesyak/ComplimentsSection.java
@@ -0,0 +1,13 @@
+package com.example.demouser.yikesyak;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ComplimentsSection extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.compliments_main);
+    }
+}
