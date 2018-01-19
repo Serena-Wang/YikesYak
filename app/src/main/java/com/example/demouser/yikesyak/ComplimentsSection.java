7ccf1f81609109abef8a9b371ee3664b8463f24f
Nada Al-Thawr <altha22n@mtholyoke.edu>
2018-01-19 13:42:13 -0500

    Merge branches 'PostBranch' and 'master' of https://github.com/Serena-Wang/YikesYak into DataStructure
    # Conflicts:
    #	app/src/main/java/com/example/demouser/yikesyak/QASection.java

--- app/src/main/java/com/example/demouser/yikesyak/ComplimentsSection.java
+++ app/src/main/java/com/example/demouser/yikesyak/ComplimentsSection.java
@@ -1,7 +1,11 @@
 package com.example.demouser.yikesyak;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class ComplimentsSection extends AppCompatActivity {
 
